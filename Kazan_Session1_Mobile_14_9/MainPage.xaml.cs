ff97dbc4834b37b5d907e8517293b0bb8c80689f
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-09-14 11:05:13 +0800

    Completed API class

--- Kazan_Session1_Mobile_14_9/MainPage.xaml.cs
+++ Kazan_Session1_Mobile_14_9/MainPage.xaml.cs
@@ -14,5 +14,35 @@ namespace Kazan_Session1_Mobile_14_9
         {
             InitializeComponent();
         }
+
+        private void dpStart_DateSelected(object sender, DateChangedEventArgs e)
+        {
+
+        }
+
+        private void dpEnd_DateSelected(object sender, DateChangedEventArgs e)
+        {
+
+        }
+
+        private void searchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMove_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHistory_Clicked(object sender, EventArgs e)
+        {
+
+        }
     }
 }
---
+++ Kazan_Session1_Mobile_14_9/WebApi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kazan_Session1_Mobile_14_9
+{
+    public class WebApi
+    {
+        string mainSite = "http://10.0.2.2:54165/";
+
+        public async Task<string> PostAsync(string Data, string extSite)
+        {
+            var client = new HttpClient();
+            var requestWeb = mainSite + extSite;
+            var response = "";
+            if (Data == null)
+            {
+                var emptyContent = new StringContent("", Encoding.UTF8, "application/json");
+                response = await client.PostAsync(requestWeb, emptyContent).Result.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                var jsonContent = new StringContent(Data, Encoding.UTF8, "application/json");
+                response = await client.PostAsync(requestWeb, jsonContent).Result.Content.ReadAsStringAsync();
+            }
+            return response;
+        }
+    }
+}
